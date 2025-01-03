68399eead28ec45e17d1dbd8bedf8dc148bee5a8
Alex <sasha.shustov.05@mail.ru>
2025-01-03 16:32:28 +0300

    initial commit

---
+++ src/components/ShopItemClass.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class ShopItemClass extends React.Component {
+    render() {
+        const { brand, title, description, descriptionFull, currency, price } = this.props;
+
+        return <div class="main-content" >
+            <h2>{brand}</h2>
+            <h1>{title}</h1>
+            <h3>{description}</h3>
+            <div class="description">
+                {descriptionFull}
+            </div>
+            <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+            <div class="divider"></div>
+            <div class="purchase-info">
+                <div class="price">{currency}{price}</div>
+                <button>Добавить в корзину</button>
+            </div>
+        </div>
+    }
+};
+
+export default ShopItemClass;
